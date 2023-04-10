83ae4907eae0525b15388bf2c07240062335d4d9
JVelezFD <jvelez117@gmail.com>
2023-04-10 10:42:11 -0400

    initial push

---
+++ team_selector.py
@@ -0,0 +1,15 @@
+import random
+
+print("Welcome to Team Allocator!")
+
+players = [
+    "Martin","Craig","Sue",
+    "Claire", "Dave", "Alice",
+    "Lucianna", "Harry", "Jack",
+    "Rose", "Lexi", "Maria",
+    "Thomas", "James", "William",
+    "Ada", "Grace", "Jean",
+    "Marissa", "Alan",
+];
+
+random.shuffle(players);
